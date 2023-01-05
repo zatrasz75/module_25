4d911af70086a585cca0c910259805273aded4c3
zatrasz75 <zatrasz@ya.ru>
2023-01-05 18:15:50 +0300

    Внес изменения в main и  readme

--- main.go
+++ main.go
@@ -6,11 +6,11 @@ import (
 )
 
 func main() {
-	n := 0
+	var n string
 	fmt.Print(" Введите данные: ")
 	_, err := fmt.Scan(&n)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("Вы ввели следующие данные: %d\n", n)
+	fmt.Printf("Вы ввели следующие данные: %v\n", n)
 }
